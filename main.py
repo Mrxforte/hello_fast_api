26735664931672601b1bba8851f230fd540ac699
MrxForte <urazimbetovazamat6@gmail.com>
2026-04-02 12:26:40 -0700

    Refactor index endpoint to be asynchronous and add delay simulation

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, Path
-
+import asyncio
 app = FastAPI(debug=True, title="My API", description="This is a sample API", version="1.0.0")
 @app.get("/")
-def index():
-    return {"message": "Hello World"} 
+async def index():
+    await asyncio.sleep(5)  # Simulate a delay
+    return {"message": "Hello World", "description": "You are waited 5 seconds to see this message"} 
 
 @app.get("/second")
 async def second():
+    await asyncio.sleep(5)  # Simulate a delay
     return {"message": "This is the second endpoint"}
